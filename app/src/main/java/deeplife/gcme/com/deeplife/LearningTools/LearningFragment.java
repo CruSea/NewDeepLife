f9260d75b849811cfa6dd1c3d37f46756eac377e
Mark Briggs <briggsm@alumni.msoe.edu>
2016-12-16 16:15:42 +0300

    Added code/layout to display 'Learning' tab.

--- app/src/main/java/deeplife/gcme/com/deeplife/LearningTools/LearningFragment.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/LearningTools/LearningFragment.java
@@ -1,11 +1,16 @@
 package deeplife.gcme.com.deeplife.LearningTools;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 import deeplife.gcme.com.deeplife.R;
 
@@ -14,6 +19,11 @@ import deeplife.gcme.com.deeplife.R;
  */
 
 public class LearningFragment extends Fragment {
+    private static RecyclerView myRecyclerView;
+    private static RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static Context myContext;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +32,20 @@ public class LearningFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.homefragment,container,false);
+        View view = inflater.inflate(R.layout.learning_fragement_page,container,false);
+        myRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        myRecyclerView.setLayoutManager(mLayoutManager);
+        myContext = getActivity();
+        ArrayList<LearningTool> items = new ArrayList<LearningTool>();
+        items.add(new LearningTool());
+        items.add(new LearningTool());
+        items.add(new LearningTool());
+        items.add(new LearningTool());
+        items.add(new LearningTool());
+        items.add(new LearningTool());
+        mAdapter = new LearningToolsListAdpapter(items, getContext());
+        myRecyclerView.setAdapter(mAdapter);
         return view;
     }
 }
--- app/src/main/java/deeplife/gcme/com/deeplife/LearningTools/LearningToolsListAdpapter.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/LearningTools/LearningToolsListAdpapter.java
@@ -2,6 +2,7 @@ package deeplife.gcme.com.deeplife.LearningTools;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import deeplife.gcme.com.deeplife.News.NewsListAdapter;
 import deeplife.gcme.com.deeplife.R;
 
 /**
@@ -26,7 +28,9 @@ public class LearningToolsListAdpapter extends RecyclerView.Adapter<LearningTool
 
     @Override
     public DataObjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.learning_fragment_item, parent, false);
+        LearningToolsListAdpapter.DataObjectHolder dataObjectHolder = new LearningToolsListAdpapter.DataObjectHolder(view);
+        return dataObjectHolder;
     }
 
     @Override
@@ -41,11 +45,11 @@ public class LearningToolsListAdpapter extends RecyclerView.Adapter<LearningTool
 
     public static class DataObjectHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
         TextView Title,Content;
-        ImageView NewsImage;
+        ImageView LearningImage;
         public DataObjectHolder(View itemView) {
             super(itemView);
-            Title = (TextView) itemView.findViewById(R.id.txt_newsfeed_title);
-            Content = (TextView) itemView.findViewById(R.id.txt_testimony_content);
+            Title = (TextView) itemView.findViewById(R.id.txt_learning_title);
+            Content = (TextView) itemView.findViewById(R.id.txt_learning_content);
             itemView.setOnClickListener(this);
             itemView.setOnLongClickListener(this);
         }
