9b69a33acf90195bc8aeea8c175f4de8b3e23610
Mark Briggs <briggsm@alumni.msoe.edu>
2016-12-21 13:24:19 +0300

    Added basic Profile Show/Edit functionality

--- app/src/main/java/deeplife/gcme/com/deeplife/MainActivity.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/MainActivity.java
@@ -5,36 +5,32 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import deeplife.gcme.com.deeplife.Adapters.ViewPageAdapter;
 import deeplife.gcme.com.deeplife.Disciples.DisciplesFragment;
 import deeplife.gcme.com.deeplife.Home.HomeFragment;
 import deeplife.gcme.com.deeplife.LearningTools.LearningFragment;
 import deeplife.gcme.com.deeplife.News.NewsFragment;
+import deeplife.gcme.com.deeplife.Profile.ProfileShowActivity;
 import deeplife.gcme.com.deeplife.SyncService.SyncService;
-import deeplife.gcme.com.deeplife.Tabs.SlidingTabLayout;
 import deeplife.gcme.com.deeplife.Testimony.TestimonyFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -160,15 +156,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.nav_home) {
             myViewPager.setCurrentItem(0,true);
         } else if (id == R.id.nav_testimony) {
-            myViewPager.setCurrentItem(1,true);
-        } else if (id == R.id.nav_news) {
             myViewPager.setCurrentItem(2,true);
+        } else if (id == R.id.nav_news) {
+            myViewPager.setCurrentItem(3,true);
         } else if (id == R.id.nav_disciples) {
-            myViewPager.setCurrentItem(3,true);
+            myViewPager.setCurrentItem(1,true);
         } else if (id == R.id.nav_learning) {
-            myViewPager.setCurrentItem(3,true);
+            myViewPager.setCurrentItem(4,true);
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_profile) {
+            startActivity(new Intent(this, ProfileShowActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
