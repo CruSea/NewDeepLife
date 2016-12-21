d97604ff970d12eceb1978ea813b808829b5b760
BENGEOS <george.beng@gmail.com>
2016-12-21 21:20:57 +0200

    add winbuildsend implementations

--- app/src/main/java/deeplife/gcme/com/deeplife/WinBuildSend/WinBuildSendActivity.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/WinBuildSend/WinBuildSendActivity.java
@@ -6,9 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import deeplife.gcme.com.deeplife.Database.Database;
 import deeplife.gcme.com.deeplife.DeepLife;
 import deeplife.gcme.com.deeplife.Disciples.Disciple;
 import deeplife.gcme.com.deeplife.Disciples.DiscipleListAdapter;
@@ -23,6 +27,8 @@ public class WinBuildSendActivity extends AppCompatActivity {
     private static RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static Context myContext;
+    private Button Btn_Win,Btn_Build,Btn_Send;
+    private String DisciplePhone;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +37,55 @@ public class WinBuildSendActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         myRecyclerView.setLayoutManager(mLayoutManager);
         myContext = this;
-        ArrayList<WinBuildSend> items = new ArrayList<WinBuildSend>();
-        items.add(new WinBuildSend());
-        items.add(new WinBuildSend());
-        items.add(new WinBuildSend());
-        mAdapter = new WinBuildSendItemsAdapter(items,this);
-        myRecyclerView.setAdapter(mAdapter);
+
+        Btn_Win = (Button) findViewById(R.id.btn_winbuildsend_win);
+        Btn_Build = (Button) findViewById(R.id.btn_winbuildsend_build);
+        Btn_Send = (Button) findViewById(R.id.btn_winbuildsend_send);
+
+        Btn_Build.setEnabled(false);
+        Btn_Send.setEnabled(false);
+        int sum = DeepLife.myDATABASE.count(Database.Table_QUESTION_LIST);
+        Toast.makeText(this,"There are: "+sum,Toast.LENGTH_LONG).show();
+        ArrayList<WinBuildSendQuestion> WinQuestioons = DeepLife.myDATABASE.getWinBuildSendQuestionsByCategorySerID(1);
+
+        DisciplePhone = getIntent().getExtras().getString("DisciplePhone");
+
+        if(WinQuestioons != null){
+            mAdapter = new WinBuildSendItemsAdapter(WinQuestioons,this,DisciplePhone);
+            myRecyclerView.setAdapter(mAdapter);
+        }
+
+        Btn_Win.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<WinBuildSendQuestion> WinQuestioons = DeepLife.myDATABASE.getWinBuildSendQuestionsByCategorySerID(1);
+                if(WinQuestioons != null){
+                    mAdapter = new WinBuildSendItemsAdapter(WinQuestioons,myContext,DisciplePhone);
+                    myRecyclerView.setAdapter(mAdapter);
+                }
+            }
+        });
+
+        Btn_Build.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<WinBuildSendQuestion> BuildQuestions = DeepLife.myDATABASE.getWinBuildSendQuestionsByCategorySerID(2);
+                if(BuildQuestions != null){
+                    mAdapter = new WinBuildSendItemsAdapter(BuildQuestions,myContext,DisciplePhone);
+                    myRecyclerView.setAdapter(mAdapter);
+                }
+            }
+        });
+        Btn_Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<WinBuildSendQuestion> SendQuestions = DeepLife.myDATABASE.getWinBuildSendQuestionsByCategorySerID(3);
+                if(SendQuestions != null){
+                    mAdapter = new WinBuildSendItemsAdapter(SendQuestions,myContext,DisciplePhone);
+                    myRecyclerView.setAdapter(mAdapter);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/deeplife/gcme/com/deeplife/WinBuildSend/WinBuildSendItemsAdapter.java
+++ app/src/main/java/deeplife/gcme/com/deeplife/WinBuildSend/WinBuildSendItemsAdapter.java
@@ -10,11 +10,14 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.List;
 
+import deeplife.gcme.com.deeplife.DeepLife;
 import deeplife.gcme.com.deeplife.Disciples.Disciple;
 import deeplife.gcme.com.deeplife.Disciples.DiscipleListAdapter;
+import deeplife.gcme.com.deeplife.Models.Answer;
 import deeplife.gcme.com.deeplife.R;
 
 /**
@@ -22,12 +25,14 @@ import deeplife.gcme.com.deeplife.R;
  */
 
 public class WinBuildSendItemsAdapter extends RecyclerView.Adapter<WinBuildSendItemsAdapter.DataObjectHolder> {
-    private List<WinBuildSend> winBuildSends;
+    public static List<WinBuildSendQuestion> winBuildSends;
     public static Context myContext;
+    public static String DisciplePhone;
 
-    public WinBuildSendItemsAdapter(List<WinBuildSend> winBuildSends,Context context) {
+    public WinBuildSendItemsAdapter(List<WinBuildSendQuestion> winBuildSends,Context context, String disciplePhone) {
         this.winBuildSends = winBuildSends;
         myContext = context;
+        DisciplePhone = disciplePhone;
     }
 
     @Override
@@ -39,12 +44,20 @@ public class WinBuildSendItemsAdapter extends RecyclerView.Adapter<WinBuildSendI
 
     @Override
     public void onBindViewHolder(DataObjectHolder holder, int position) {
+        Answer answer = DeepLife.myDATABASE.getAnswerByQuestionIDandDisciplePhone(winBuildSends.get(position).getSerID(),DisciplePhone);
         if(position%2 == 0){
             holder.frameLayout1.setVisibility(View.GONE);
             holder.frameLayout2.setVisibility(View.VISIBLE);
+            holder.Question2.setText(winBuildSends.get(position).getQuestion());
+            if(answer != null){
+                holder.QuestionValue.setText(""+answer.getAnswer());
+            }
+
         }else {
             holder.frameLayout1.setVisibility(View.VISIBLE);
             holder.frameLayout2.setVisibility(View.GONE);
+            holder.Question1.setText(winBuildSends.get(position).getQuestion());
+
         }
     }
 
@@ -54,20 +67,59 @@ public class WinBuildSendItemsAdapter extends RecyclerView.Adapter<WinBuildSendI
     }
 
     public static class DataObjectHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
-        TextView FullName,Email,Phone;
-        Button btn_WinBuild;
+        TextView Question1,Question2,Question3,Email,Phone;
+        Button btnInc,btnDec;
+        ToggleButton btnToggle;
         ImageView NewsImage;
+        TextView QuestionValue;
         FrameLayout frameLayout1,frameLayout2;
         public DataObjectHolder(View itemView) {
             super(itemView);
             frameLayout1 = (FrameLayout) itemView.findViewById(R.id.frame1);
             frameLayout2 = (FrameLayout) itemView.findViewById(R.id.frame2);
+
+            Question1 = (TextView) itemView.findViewById(R.id.txt_winbuildsend_question1);
+            Question2 = (TextView) itemView.findViewById(R.id.txt_winbuildsend_question2);
+
+            btnInc = (Button) itemView.findViewById(R.id.btn_winbuildsend_inc);
+            btnInc.setOnClickListener(this);
+            btnDec = (Button) itemView.findViewById(R.id.btn_winbuildsend_dec);
+            btnDec.setOnClickListener(this);
+
+            QuestionValue = (TextView) itemView.findViewById(R.id.txt_winbuildsend_value);
             itemView.setOnClickListener(this);
             itemView.setOnLongClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+            Answer answer = new Answer();
+            if(v.getId() == R.id.btn_winbuildsend_inc){
+                int value = Integer.valueOf(QuestionValue.getText().toString());
+                value = value + 1;
+                QuestionValue.setText(""+value);
+                answer.setDisciplePhone(DisciplePhone);
+                answer.setQuestionID(winBuildSends.get(getAdapterPosition()).getSerID());
+                answer.setAnswer(""+value);
+                answer.setSerID(0);
+                answer.setBuildStage("WIN");
+                DeepLife.myDATABASE.add_updateAnswer(answer);
+            }else if(v.getId() == R.id.btn_winbuildsend_dec){
+                int value = Integer.valueOf(QuestionValue.getText().toString());
+                if(value > 0){
+                    value = value - 1;
+                    QuestionValue.setText(""+value);
+                    answer.setDisciplePhone(DisciplePhone);
+                    answer.setQuestionID(winBuildSends.get(getAdapterPosition()).getSerID());
+                    answer.setAnswer(""+value);
+                    answer.setSerID(0);
+                    answer.setBuildStage("WIN");
+                    DeepLife.myDATABASE.add_updateAnswer(answer);
+                }
+
+            }
+
+
 
         }
 
