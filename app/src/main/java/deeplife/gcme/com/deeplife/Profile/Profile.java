36cd07473a4f96ebae9644ad660cd034ec537444
BENGEOS <george.beng@gmail.com>
2017-04-08 14:05:08 +0300

    add new profile model

---
+++ app/src/main/java/deeplife/gcme/com/deeplife/Profile/Profile.java
@@ -0,0 +1,57 @@
+package deeplife.gcme.com.deeplife.Profile;
+
+/**
+ * Created by bengeos on 4/8/17.
+ */
+
+public class Profile {
+    private String id,email,fullname,country,gender,phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
